9e8d5f0c12fb07d13767b9af45143f8e836b3395
tocoolforskool <jessiecryer@Outlook.com>
2025-05-28 23:07:31 -0500

    add chakra-ui

---
+++ src/components/ui/provider.tsx
@@ -0,0 +1,16 @@
+// don't need - but can use as <Provider> wrapper in main.tsx
+// import { Provider } from './components/ui/provider' <--> add this to main.tsx
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import type { ReactNode } from "react"
+
+interface ProviderProps {
+    children: ReactNode
+}
+
+export function Provider({ children }: ProviderProps) {
+    return (
+        <ChakraProvider value={defaultSystem}>
+            {children}
+        </ChakraProvider>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ChakraProvider, defaultSystem, Button, ButtonGroup } from "@chakra-ui/react"
+import { Provider } from './components/ui/provider'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ChakraProvider value={defaultSystem}>
+      <App />
+    </ChakraProvider>
   </StrictMode>,
 )
