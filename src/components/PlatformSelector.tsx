cc001a07bc01ce9d637a2cb10937267d729398e2
tocoolforskool <jessiecryer@Outlook.com>
2025-07-07 21:03:44 -0500

    implement platforms filter

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,9 +1,13 @@
-import usePlatforms from "../hooks/usePlatforms";
+import usePlatforms, { type Platform } from "../hooks/usePlatforms";
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
-import React from "react";
 import { BsChevronDown } from "react-icons/bs";
 
-const PlatformSelector = () => {
+interface Props {
+    onSelectPlatform: (platform: Platform) => void;
+    selectedPlatform: Platform | null;
+}
+
+const PlatformSelector = ({onSelectPlatform, selectedPlatform}: Props) => {
   const { data, error } = usePlatforms();
 
   if (error) return null;
@@ -11,11 +15,11 @@ const PlatformSelector = () => {
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        Platform
+        {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
         {data.map((platform) => (
-          <MenuItem key={platform.id}>{platform.name}</MenuItem>
+          <MenuItem onClick={() => onSelectPlatform(platform)} key={platform.id}>{platform.name}</MenuItem>
         ))}
       </MenuList>
     </Menu>
